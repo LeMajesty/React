f22a8c6464f8ba1eb783297ae02ba3195a57a080
LeMajesty <148125836+LeMajesty@users.noreply.github.com>
2024-09-24 11:54:24 -0400

    Add files via upload

---
+++ EventandSlate.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+
+import "./EightBall.css";
+import defaultAnswers from "./answers.json";
+import { choice } from "./random";
+
+
+function EightBall({ answers = defaultAnswers }) {
+  const [answer, setAnswer] = useState({
+    msg: "Think of a Question.",
+    color: "black",
+  });
+
+  function handleClick(evt) {
+    setAnswer(choice(answers));
+  }
+
+  return (
+      <div
+          className="EightBall"
+          onClick={handleClick}
+          style={{ backgroundColor: answer.color }}
+      >
+        <b>{answer.msg}</b>
+      </div>
+  );
+}
+
+
+export default EightBall;
+
+.EightBall {
+  box-shadow: 5px 5px 10px #678;
+  margin: 50px;
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+}
+
+import React, { useState } from "react";
+import Box from "./Box";
+import "./BoxesContainer.css";
+import { choice, randInt } from "./random";
+
+const defaultColors = [
+  "Aqua", "Black", "BlanchedAlmond", "Blue", "Chocolate", "DodgerBlue",
+  "Lavender", "LawnGreen", "Peru", "Plum", "SpringGreen", "SteelBlue", "Tan",
+  "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Yellow", "YellowGreen",
+];
+
+/** Container for set of colored boxes.
+ *
+ * Props:
+ * - numBoxes: # of boxes to manage
+ * - allColors: array of random colors to choose
+ *
+ * State:
+ * - boxes: array of box color strings
+ */
+
+function BoxesContainer({ allColors = defaultColors, numBoxes = 16 }) {
+  // note: passes function, so this can be only called once at start
+  const [boxes, setBoxes] = useState(getInitialRandomColors);
+
+  /** Return array of random colors. */
+  function getInitialRandomColors() {
+    return Array.from(
+        { length: numBoxes },
+        () => choice(allColors));
+  }
+
+  /** On button click: pick random box, change to random color. */
+  function handleClick(evt) {
+    setBoxes(boxes => {
+      let idx = randInt(numBoxes);
+      let boxCopy = [...boxes];
+      boxCopy[idx] = choice(allColors);
+      return boxCopy;
+    });
+  }
+
+  const boxComponents = boxes.map((color, i) => <Box key={i} color={color} />);
+
+  return (
+      <div>
+        <section className="BoxesContainer">{boxComponents}</section>
+        <button onClick={handleClick}>Change a Box</button>
+      </div>
+  );
+}
+
+
+export default BoxesContainer;
+
+import React from "react";
+import "./Box.css";
+
+
+function Box({ color }) {
+  return (
+      <div
+          className="Box"
+          style={{ backgroundColor: color }}
+      />
+  );
+}
+
+export default Box;
